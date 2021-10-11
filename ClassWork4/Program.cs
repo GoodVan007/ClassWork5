1a80a3b20a9d162a938bbff8300c1a904aad3e90
Будаев Иван <budaevivan2003@gmail.com>
2021-10-11 13:32:17 +0500

    Добавьте файлы проекта.

---
+++ ClassWork4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ClassWork4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetHalfString("Hello, world"));
+            Console.WriteLine(GetHalfString(""));
+            Console.WriteLine(GetHalfString("A B C D") +'#');
+            Console.WriteLine(GetHalfString("                            ") + '#');
+
+            Console.ReadKey();
+
+
+        }
+
+        static string GetHalfString(string fullString)
+        {
+            var s = fullString.Replace(" ", "");
+            return s.Substring(s.Length / 2);
+
+        }
+
+
+
+    }
+}
